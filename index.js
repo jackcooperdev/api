b301d4eab4a2eb229670d9b0907551762c8a1fb6
Jack Cooper <dev@jackcooper.me>
2024-10-05 17:18:45 +0100

    prodReady

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
+const envToLogger = {
+    development: {
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          translateTime: 'HH:MM:ss Z',
+          ignore: 'pid,hostname',
+        },
+      },
+    },
+    production: true,
+    test: false,
+  }
+
 const fastify = require('fastify')({
-    logger: false
+    logger: envToLogger['development'] ?? true // defaults to true if no entry matches in the map
 })
 
 const jamesRoute = require('./routes/james');
 
 // Declare a route
 fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'world' })
+    reply.send(200)
 })
 
-fastify.register(jamesRoute, {prefix:'james'})
+fastify.register(jamesRoute, {prefix:'james_portfolio'})
 
 // Run the server!
 fastify.listen({ port: 3000 }, function (err, address) {
