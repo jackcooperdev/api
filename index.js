2cf96a749d705369148326fd15ff84fdc2eebbef
Jack Cooper <dev@jackcooper.me>
2024-11-24 19:18:31 +0000

    :rocket: Port Fix

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ fastify.register(hackathonRoute, {prefix:'hackathon'});
 
 
 // Run the server!
-fastify.listen({ port: 4000 }, function (err, address) {
+fastify.listen({ port: 3000 }, function (err, address) {
     if (err) {
         fastify.log.error(err)
         process.exit(1)
